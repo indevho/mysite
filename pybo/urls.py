20241abe8b197b8aba0c0828b3211da0344cb7cc
inDev <unoestellar@naver.com>
2022-09-19 00:28:44 +0900

    2-06 데이터저장,detail에서 엮인답변들확인

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,14 +2,34 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
 
+
+app_name = 'pybo'
+'''
+ pybo 이외의 앱이 추가되는 경우도 상정하라! 
+
+'''
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>', views.answer_create, name='answer_create'),
 ]
 '''
 url pattern 중 
  string 으로 들어오면? 
  <string:search_keyword>/ 로 가야 하나? 
+
+'''
+'''
+   URL 하드코딩 을 극복하는 URL 별칭 사용을 위하여, 
+    path의 파라미터로  name 이 추가된다!
+    
+    
+    redirect 함수와 URL 별칭
+URL별칭은 템플릿 외에 redirect 함수에서도 사용된다. 
+redirect는 특정 페이지로 이동시키는 함수이다. 
+redirect 함수의 사용은 뒤에서 자세히 공부한다.
+redirect('pybo:detail', question_id=question.id)
 
 '''
--- pybo/views.py
+++ pybo/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 # version1  from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
-
+from .models import Answer
 #version1  def index(request):
 #version1    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 
@@ -49,3 +50,12 @@ def detail(request, question_id):
 
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    #answer버전!
+    #answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    #answer.save()
+
+    return redirect('pybo:detail', question_id=question.id)
